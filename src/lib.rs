5df68e85630c31015db9d30f7c403d9e3215ce31
unrealhoang <unrealhoang@gmail.com>
2021-06-18 21:15:42 +0900

    add struct HuyTD
    betrays everything

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
 pub trait Or<T> {
     fn betray(&self, thing: T) -> bool;
 }
+
+pub struct HuyTD {};
+
+impl<T> Or<T> for HuyTD
+where T: Any {
+    fn betray(&self, thing: T) -> bool { true };
+}
